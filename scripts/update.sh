733cf48231cb69a3b1991279240bcf2efdc7c395
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-10-16 12:03:25 +0200

    also start running for 2.12 already now

--- scripts/update.sh
+++ scripts/update.sh
@@ -14,3 +14,5 @@ echo "Running at $(date) for 2.11 -> 2.12.0-RC1" | tee -a $LOG
 java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12.0-RC1 | gist -f analysis.txt -u d51a2f859428e54bbd7b7909250b8611 2>&1 | tee -a $LOG
 echo "Running at $(date) for 2.11 -> 2.12.0-RC2" | tee -a $LOG
 java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12.0-RC2 | gist -f analysis.txt -u 749811a5a205824f864cdd6a06352447 2>&1 | tee -a $LOG
+echo "Running at $(date) for 2.11 -> 2.12" | tee -a $LOG
+java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12.0 | gist -f analysis.txt -u e058d52bf7d89d9c4e2602aed4fca6f3 2>&1 | tee -a $LOG
