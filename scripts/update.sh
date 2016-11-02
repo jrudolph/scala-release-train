3612cb7c4f7ab1aae40b3acb86c88e7257831144
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-11-02 14:02:56 +0100

    get rid of old release train updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,13 +6,5 @@ LOG=$DIR/run/output.log
 cd $DIR/run
 echo "Running at $(date) for 2.10 -> 2.11" | tee -a $LOG
 java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.10 -t 2.11 | gist -f analysis.txt -u 7a323f5e2820d8479b18 2>&1 | tee -a $LOG
-echo "Running at $(date) for 2.11 -> 2.12.0-M3" | tee -a $LOG
-java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12.0-M3 | gist -f analysis.txt -u d73062db6044fa0a8933 2>&1 | tee -a $LOG
-echo "Running at $(date) for 2.11 -> 2.12.0-M5" | tee -a $LOG
-java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12.0-M5 | gist -f analysis.txt -u d0e20c4a20af44459de1 2>&1 | tee -a $LOG
-echo "Running at $(date) for 2.11 -> 2.12.0-RC1" | tee -a $LOG
-java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12.0-RC1 | gist -f analysis.txt -u d51a2f859428e54bbd7b7909250b8611 2>&1 | tee -a $LOG
-echo "Running at $(date) for 2.11 -> 2.12.0-RC2" | tee -a $LOG
-java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12.0-RC2 | gist -f analysis.txt -u 749811a5a205824f864cdd6a06352447 2>&1 | tee -a $LOG
 echo "Running at $(date) for 2.11 -> 2.12" | tee -a $LOG
 java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12 | gist -f analysis.txt -u e058d52bf7d89d9c4e2602aed4fca6f3 2>&1 | tee -a $LOG
