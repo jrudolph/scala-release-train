44a85a01bac1b994d3f42bd68a58b29adb59473f
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-04-24 09:39:00 +0200

    Update for 2.13.0-RC1

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,5 +8,5 @@ echo "Running at $(date) for 2.10 -> 2.11" | tee -a $LOG
 java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.10 -t 2.11 | gist -f analysis.txt -u 7a323f5e2820d8479b18 2>&1 | tee -a $LOG
 echo "Running at $(date) for 2.11 -> 2.12" | tee -a $LOG
 java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.11 -t 2.12 | gist -f analysis.txt -u e058d52bf7d89d9c4e2602aed4fca6f3 2>&1 | tee -a $LOG
-echo "Running at $(date) for 2.12 -> 2.13.0-M5" | tee -a $LOG
-java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.12 -t 2.13.0-M5 | gist -f analysis.txt -u 1b054191c235469592f62c7c61cb657d 2>&1 | tee -a $LOG
+echo "Running at $(date) for 2.12 -> 2.13.0-RC1" | tee -a $LOG
+java -jar /tmp/release-train-main-assembly-0.1-SNAPSHOT.jar -q -l 2.12 -t 2.13.0-RC1 | gist -f analysis.txt -u 1b054191c235469592f62c7c61cb657d 2>&1 | tee -a $LOG
